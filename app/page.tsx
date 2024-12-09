83f282632e4163735c4c926c1fc770faaf970be3
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-12-09 23:56:45 +0530

    chore: added github auth and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@nextui-org/button";
 
+import SignIn from "@/components/sign-in";
+
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <Button>Hello</Button>
+      <SignIn />
     </section>
   );
 }
